514fdc924c9a86602a73cb99c17b27b92ded99be
yanbierhals <yanrbierhals@gmail.com>
2023-07-03 01:08:28 -0300

    interface

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VendedoresComponent } from './components/vendedores/vendedores.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
+import { ProdutosComponent } from './components/produtos/produtos.component';
+import { PedidosComponent } from './components/pedidos/pedidos.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'vendedores', component: VendedoresComponent },
+      { path: 'clientes', component: ClientesComponent },
+      { path: 'produtos', component: ProdutosComponent },
+      { path: 'pedidos', component: PedidosComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { VendedoresComponent } from './components/vendedores/vendedores.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
+import { ProdutosComponent } from './components/produtos/produtos.component';
+import { PedidosComponent } from './components/pedidos/pedidos.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    LayoutComponent,
+    AppComponent,
+    VendedoresComponent,
+    ClientesComponent,
+    ProdutosComponent,
+    PedidosComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
